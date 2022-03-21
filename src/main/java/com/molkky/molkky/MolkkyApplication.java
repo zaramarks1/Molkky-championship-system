a6b4c4ae17a2a09d80f45b0120ad3babcfe04868
Lucien <voltzlucien@gmail.com>
2022-03-21 09:57:52 +0100

    init projet

---
+++ src/main/java/com/molkky/molkky/MolkkyApplication.java
@@ -0,0 +1,13 @@
+package com.molkky.molkky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolkkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolkkyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molkky/molkky/MolkkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molkky.molkky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolkkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
