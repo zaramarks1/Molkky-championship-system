7d28e5a2eb44905bcc419374c17b8268065d8bd5
Lucien <voltzlucien@gmail.com>
2022-03-22 15:41:21 +0100

    🚨 Added test for team entity creation

--- src/main/java/com/molkky/molkky/MolkkyApplication.java
+++ src/main/java/com/molkky/molkky/MolkkyApplication.java
@@ -1,6 +1,5 @@
 package com.molkky.molkky;
 
-import com.molkky.molkky.domain.Court;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MolkkyApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MolkkyApplication.class, args);
-
 	}
 
 }
