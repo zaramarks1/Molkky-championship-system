ef964afdf22e7bf48e75ad105b42ab2896c9a8e3
Lucien <voltzlucien@gmail.com>
2022-03-23 10:49:25 +0100

    🚨 Pool creation test

--- src/test/java/com/molkky/molkky/PoolEntityTest.java
+++ src/test/java/com/molkky/molkky/PoolEntityTest.java
@@ -9,9 +9,18 @@ import com.molkky.molkky.repository.PoolRepository;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Propagation;
 
+import javax.transaction.Transactional;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -25,16 +34,24 @@ class PoolEntityTest {
     private MatchRepository matchRepository;
 
     @Test
+    @Transactional
     void testInsertPool() {
-        Match match = matchRepository.save(new Match());
-        Match match2 = matchRepository.save(new Match());
+        Match match = new Match();
+        Match match2 = new Match();
         List<Match> matches = Arrays.asList(match, match2);
 
         Pool pool = new Pool();
+        match.setPool(pool);
+        match2.setPool(pool);
         pool.setMatches(matches);
-        poolRepository.save(pool);
+
+        pool = poolRepository.save(pool);
+        System.out.println(pool.getMatches());
         Assertions.assertNotNull(pool.getId());
-        Match recupMatch = matchRepository.findByPool(pool);
+        Match recupMatch = matchRepository.findById(match.getId());
         Assertions.assertEquals(recupMatch.getPool().getId(), pool.getId());
+
+        pool = poolRepository.findById(pool.getId());
+        Assertions.assertEquals(2, pool.getMatches().size());
     }
 }
