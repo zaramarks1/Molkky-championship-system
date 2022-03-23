d8d5e6e3e4f31ba0a174f546d43855b8aa6a57da
Lucien <voltzlucien@gmail.com>
2022-03-23 15:31:34 +0100

    ✨ Finnish entity + relation

---
+++ src/main/java/com/molkky/molkky/domain/Finnish.java
@@ -0,0 +1,37 @@
+package com.molkky.molkky.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Setter
+@Table(name = "finnish")
+public class Finnish {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nbFinnish")
+    private Integer nbFinnish;
+
+    @Column(name = "nbTeamsQualified")
+    private Integer nbTeamsQualified;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idFinnish")
+    private List<Match> matches = new ArrayList<>();
+
+    public Finnish(){
+    }
+
+    public Finnish(Integer nbFinnish, Integer nbTeamsQualified){
+        this.nbFinnish = nbFinnish;
+        this.nbTeamsQualified = nbTeamsQualified;
+    }
+}
