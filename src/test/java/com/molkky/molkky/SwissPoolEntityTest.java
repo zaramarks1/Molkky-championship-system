9c889410136a45390be599fe37586c9e0ba1da44
Lucien <voltzlucien@gmail.com>
2022-03-23 15:23:43 +0100

    🚨 Swisspool test

---
+++ src/test/java/com/molkky/molkky/SwissPoolEntityTest.java
@@ -0,0 +1,44 @@
+package com.molkky.molkky;
+
+import com.molkky.molkky.domain.Match;
+import com.molkky.molkky.domain.SwissPool;
+import com.molkky.molkky.repository.MatchRepository;
+import com.molkky.molkky.repository.SwissPoolRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest(classes = MolkkyApplication.class)
+class SwissPoolEntityTest {
+    @Autowired
+    private SwissPoolRepository swissPoolRepository;
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @Test
+    @Transactional
+    void testInsertSimpleGame() {
+        Match match = new Match();
+        Match match2 = new Match();
+        List<Match> matches = Arrays.asList(match, match2);
+
+        SwissPool swissPool = new SwissPool(2, 2);
+        match.setSwissPool(swissPool);
+        match2.setSwissPool(swissPool);
+        swissPool.setMatches(matches);
+
+        swissPool = swissPoolRepository.save(swissPool);
+        System.out.println(swissPool.getMatches());
+        Assertions.assertNotNull(swissPool.getId());
+        Match recupMatch = matchRepository.findById(match.getId());
+        Assertions.assertEquals(recupMatch.getSwissPool().getId(), swissPool.getId());
+
+        swissPool = swissPoolRepository.findById(swissPool.getId());
+        Assertions.assertEquals(2, swissPool.getMatches().size());
+    }
+}
