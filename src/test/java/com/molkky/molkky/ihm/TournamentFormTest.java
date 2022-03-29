735fe8d1a4fd62358b19aa53d17d534289b782aa
Lucien <voltzlucien@gmail.com>
2022-03-29 10:57:14 +0200

    🚨 Variable URL in test

--- src/test/java/com/molkky/molkky/ihm/TournamentFormTest.java
+++ src/test/java/com/molkky/molkky/ihm/TournamentFormTest.java
@@ -3,21 +3,26 @@ package com.molkky.molkky.ihm;
 import com.molkky.molkky.MolkkyApplication;
 import com.molkky.molkky.SeleniumConfig;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = MolkkyApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TournamentFormTest {
     private SeleniumConfig config;
+    @Value("${server.port}")
+    private Integer port;
+    private String url;
 
     @BeforeAll
     void setUp() {
         config = new SeleniumConfig();
+        url = String.format("http://localhost:%s", port.toString());
     }
 
     @Test
     void testTournamentForm() {
-        config.getDriver().get("http://localhost:8080/tournament/create");
+        config.getDriver().get(url + "/tournament/create");
         Assertions.assertEquals("Creation Nouveau Tournoi", config.getDriver().getTitle());
     }
 
