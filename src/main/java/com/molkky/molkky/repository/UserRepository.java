e494ebf1184ceea48980e9bbf55055bb720c6bb9
sacha <sacha.thuault@reseau.eseo.fr>
2022-03-30 10:36:06 +0200

    "Sign in first implementations"

--- src/main/java/com/molkky/molkky/repository/UserRepository.java
+++ src/main/java/com/molkky/molkky/repository/UserRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
     User findById(Integer id);
+    List<User> findAll();
 }
