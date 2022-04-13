d990defa626be59119fe0a2d63aff99247281846
Zara Marks <zara.marks@reseau.eseo.fr>
2022-04-13 09:45:33 +0200

    Change sql tournament

--- src/main/java/com/molkky/molkky/repository/TournamentRepository.java
+++ src/main/java/com/molkky/molkky/repository/TournamentRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface TournamentRepository extends JpaRepository<Tournament, String>, JpaSpecificationExecutor<Tournament> {
     Tournament findById(Integer id);
     Tournament findByName(String tournamentName);
-    Tournament findByStatus(String status);
+    List<Tournament> findByIsVisibleAndStatus(boolean isVisible, String status);
+
 }
