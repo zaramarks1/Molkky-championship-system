23061d2fd366d09a24dee14742495256eef7779f
Lucien <voltzlucien@gmail.com>
2022-05-01 12:18:19 +0200

    Chrome  -> Firefox for selenium tests in jenkins

--- src/main/java/com/molkky/molkky/SeleniumConfig.java
+++ src/main/java/com/molkky/molkky/SeleniumConfig.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 @Getter
 public class SeleniumConfig {
@@ -11,16 +13,18 @@ public class SeleniumConfig {
     private WebDriver driver;
 
     public SeleniumConfig() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-
         boolean isJenkins = System.getenv("JENKINS_HOME") != null;
         if(isJenkins) {
+            FirefoxOptions options = new FirefoxOptions();
             options.addArguments("--headless");
             options.addArguments("--disable-dev-shm-usage");
+            driver = new FirefoxDriver(options);
+        }else {
+            ChromeOptions options = new ChromeOptions();
             options.addArguments("--window-size=1920x1080");
+            options.addArguments("--no-sandbox");
+            driver = new ChromeDriver(options);
         }
-        driver = new ChromeDriver(options);
     }
 
     static {
@@ -28,6 +32,11 @@ public class SeleniumConfig {
             System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
         } else{
             System.setProperty("webdriver.chrome.driver", "bin/chromedriver99.exe");
+        }
+        if(System.getenv("FIREFOX_DRIVER") != null){
+            System.setProperty("webdriver.gecko.driver", System.getenv("FIREFOX_DRIVER"));
+        } else{
+            System.setProperty("webdriver.gecko.driver", "bin/geckodriver");
         }
     }
 }
