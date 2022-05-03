be87f61b4c5b62c6950469d7f527f69a045fd5c0
Francois Bous <franbous49@gmail.com>
2022-05-03 13:17:26 +0200

    ✔️Make test for tournament service pass with Mock

--- src/test/java/com/molkky/molkky/service/TournamentServiceTest.java
+++ src/test/java/com/molkky/molkky/service/TournamentServiceTest.java
@@ -1,74 +1,86 @@
 package com.molkky.molkky.service;
 
-import com.molkky.molkky.MolkkyApplication;
 import com.molkky.molkky.domain.Tournament;
 import com.molkky.molkky.domain.User;
+import com.molkky.molkky.domain.UserTounamentRole;
 import com.molkky.molkky.model.TournamentModel;
 import com.molkky.molkky.repository.TournamentRepository;
 import com.molkky.molkky.repository.UserRepository;
+import com.molkky.molkky.repository.UserTounamentRoleRepository;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.mockito.stubbing.OngoingStubbing;
 
-@SpringBootTest(classes = MolkkyApplication.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TournamentServiceTest {
 
-    @Autowired
+    String name = "test" + Math.random() * 10000;
+
+    @InjectMocks
     private TounamentService tounamentService;
-
-    @Autowired
-    private TournamentRepository tournamentRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Mock
     private TournamentModel tournamentModel;
 
-    @Test
-    public void testCreateCode(){
-        int length = 5;
+    @Mock
+    private UserTounamentRole userTounamentRole;
 
-        String code = this.tounamentService.createCode(length);
+    @Mock
+    private TournamentRepository tournamentRepository;
 
-        StringBuilder test = new StringBuilder();
-        for (int i =0; i<length;i++){
-            test.append("a");
-        }
+    @Mock
+    private UserRepository userRepository;
 
-        Assertions.assertEquals(test.length(), code.length());
-    }
+    @Mock
+    private UserTounamentRoleRepository userTounamentRoleRepository;
 
-    @Test
-    public void createTournamentServiceWithoutUser(){
-        String name = "test" + Math.random() * 10000;
+    @Mock
+    private Tournament tournament;
+
+    @Mock
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
         Mockito.when(tournamentModel.getName()).thenReturn(name);
-        Tournament tournament = this.tounamentService.create(this.tournamentModel);
-        Tournament tested = this.tournamentRepository.findByName(name);
-
-        Assertions.assertEquals(tournament.getName(), tested.getName());
     }
 
     @Test
-    public void createTournamentServiceWithUser(){
-        String name = "test" + Math.random() * 10000;
-        String email = "coucou@gmail.com";
-        Mockito.when(tournamentModel.getEmail()).thenReturn(email);
-        Mockito.when(tournamentModel.getName()).thenReturn(name);
-        Tournament tournament = this.tounamentService.create(this.tournamentModel);
+    public void testCreateCode() {
+        String code = this.tounamentService.createCode(5);
 
-        if (!this.userRepository.existsUserByEmail(email)){
-            User user = new User();
-            user.setEmail(email);
-            user.setPassword(this.tounamentService.createCode(5));
-            this.userRepository.save(user);
-        }
-
-        Assertions.assertEquals(tournament.getName(), this.tournamentRepository.findByName(name).getName());
-
+        Assertions.assertEquals(5, code.length());
     }
 
+    @Test
+    public void createTournamentServiceWithoutUser() {
+        Mockito.when(this.tournamentModel.getName()).thenReturn("TEST 1");
+        Mockito.when(this.tournamentRepository.save(Mockito.any(Tournament.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        Tournament test = this.tounamentService.create(this.tournamentModel);
+
+        // Vérification de la création du tournoi et du nom qui est bien assigné
+        Assertions.assertEquals("TEST 1", test.getName(), "Name incorrect");
+    }
+
+    @Test
+    public void createTournamentServiceWithUser() {
+        Mockito.when(this.tournamentModel.getName()).thenReturn("TEST 1");
+        Mockito.when(this.tournamentRepository.save(Mockito.any(Tournament.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        // On force le test vérifiant l'existence de l'utilisateur à true
+        Mockito.when(this.userRepository.existsUserByEmail(this.user.getEmail())).thenReturn(true);
+
+        Tournament test = this.tounamentService.create(this.tournamentModel);
+
+        // Vérification de la création du tournoi et du nom qui est bien assigné
+        Assertions.assertEquals("TEST 1", test.getName(), "Name incorrect");
+    }
 }
+
