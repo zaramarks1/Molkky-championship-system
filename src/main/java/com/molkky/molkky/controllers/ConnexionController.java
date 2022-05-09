14ffe2e1e6cd6b4b8e8047ceb0488ec24453f530
lucien <lucien.voltz@reseau.eseo.fr>
2022-05-09 11:49:05 +0200

    Fix connexionTest

--- src/main/java/com/molkky/molkky/controllers/ConnexionController.java
+++ src/main/java/com/molkky/molkky/controllers/ConnexionController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -39,7 +38,7 @@ public class ConnexionController {
         UserConnectionModel userConnectionModel = new UserConnectionModel();
 
         model.addAttribute("userConnection", userConnectionModel);
-        return "connexionTest";
+        return "connexion";
     }
 
     @PostMapping("/connexion")
--- src/main/java/com/molkky/molkky/controllers/HomeController.java
+++ src/main/java/com/molkky/molkky/controllers/HomeController.java
@@ -1,9 +1,10 @@
 package com.molkky.molkky.controllers;
 
-import com.molkky.molkky.domain.User;
+import com.molkky.molkky.model.UserLogged;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -11,7 +12,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String Index(Model model, HttpSession session){
-        User user = (User)session.getAttribute("user");
+        UserLogged user = (UserLogged)session.getAttribute("user");
         model.addAttribute("user", user);
         return "/home";
     }
