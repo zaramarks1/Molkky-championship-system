92dc3733f51ff56204f54cbc586b56e86f8825ee
Zara Marks <zara.marks@reseau.eseo.fr>
2022-05-12 20:23:51 +0200

    no message

--- src/main/java/com/molkky/molkky/controllers/PhaseController.java
+++ src/main/java/com/molkky/molkky/controllers/PhaseController.java
@@ -1,13 +1,22 @@
 package com.molkky.molkky.controllers;
 
+import com.molkky.molkky.domain.Match;
 import com.molkky.molkky.domain.Phase;
+import com.molkky.molkky.domain.Round;
+import com.molkky.molkky.service.MatchService;
 import com.molkky.molkky.service.PhaseService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("/phase")
@@ -15,11 +24,15 @@ public class PhaseController {
 
     @Autowired
     PhaseService phaseService;
-    @GetMapping("/{id}/generate")
+    @GetMapping("/generate")
+    public HashMap<Round, List<Match>> generate(Model model, HttpSession session, @RequestParam(name = "phase_id", required = true) String id){
 
-    public void generate(@PathVariable String id){
 
-        phaseService.generate(id);
+        HashMap<Round, List<Match>> response = phaseService.generate(id);
+
+        model.addAttribute("round_match", response);
+
+        return response;
 
     }
 
--- src/test/java/com/molkky/molkky/entity/RoundEntityTest.java
+++ src/test/java/com/molkky/molkky/entity/RoundEntityTest.java
@@ -1,18 +1,18 @@
 package com.molkky.molkky.entity;
 
+import com.molkky.molkky.domain.*;
+import com.molkky.molkky.domain.rounds.Pool;
+import com.molkky.molkky.repository.*;
+import com.molkky.molkky.service.PhaseService;
 import type.PhaseType;
 import com.molkky.molkky.MolkkyApplication;
-import com.molkky.molkky.domain.Round;
-import com.molkky.molkky.domain.Team;
-import com.molkky.molkky.domain.Tournament;
-import com.molkky.molkky.repository.RoundRepository;
-import com.molkky.molkky.repository.TeamRepository;
-import com.molkky.molkky.repository.TournamentRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import type.TournamentStatus;
+import type.UserRole;
 
 import javax.transaction.Transactional;
 import java.util.*;
@@ -20,47 +20,112 @@ import java.util.*;
 @SpringBootTest(classes = MolkkyApplication.class)
 class RoundEntityTest {
     @Autowired
-    private RoundRepository roundRepository;
-    @Autowired
-    private TeamRepository teamRepository;
-    @Autowired
     private TournamentRepository tournamentRepository;
 
-    /*@Test
-    @Transactional
+    @Autowired
+    private PhaseRepository phaseRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserTournamentRoleRepository userTournamentRoleRepository;
+
+    @Autowired
+    private PhaseService phaseService;
+
+    @Test
     @Rollback(false)
-    void testInsertUser() {
-        Tournament tournament = tournamentRepository.save(new Tournament(
-                "tournament_name",
+    @Transactional
+    void testInsertTournamentWithRound() {
+        Tournament tournament = new Tournament(
+                "tournament test",
                 "location",
                 new Date(),
                 new Date(),
                 1,
-                2,
+                8,
                 true,
                 2,
                 3
-        ));
+        );
+        tournament.setNbPlayersPerTeam(1);
+        tournament.setVisible(true);
+        tournament.setStatus(TournamentStatus.AVAILABLE);
+        tournament= tournamentRepository.save(tournament);
 
-        Round round = new Round(PhaseType.POOL, 2);
-        round.setTournament(tournament);
+        Pool pool = new Pool();
 
-        Team team1 = teamRepository.save(new Team());
-        team1.setName("team1_testMatch");
-        Team team2 = teamRepository.save(new Team());
-        team2.setName("team2_testMatch");
+        pool.setNbSets(1);
+        pool.setVictoryValue(2);
+        pool.setNbPhase(1);
+        pool.setNbPools(2);
+        pool.setNbTeamsQualified(4);
 
-        List<Team> teams = new ArrayList<>();
-        teams.add(team1);
-        teams.add(team2);
+        pool.setTournament(tournament);
+        pool =  phaseRepository.save(pool);
 
-        round.setTeams(teams);
+        List<Phase> phases = new ArrayList<>();
+        phases.add(pool);
+        tournament.setPhases(phases);
+        tournamentRepository.save(tournament);
 
-        Round newRound = roundRepository.save(round);
 
-        Assertions.assertEquals(PhaseType.POOL, round.getType(), "Type is not correct");
-        Round recupRound = roundRepository.findById(round.getId());
-        Assertions.assertEquals(recupRound.getType(), round.getType(), "Type is not correct");
-       // Assertions.assertEquals(recupRound.getTeams().size(), round.getTeams().size(), "Team size is not correct");
-    }*/
+        for (int i =1; i <= 8; i++){
+            Team team = new Team();
+
+            team.setCode("12345");
+
+            team.setName("Team" + i);
+            team.setTournament(tournament);
+
+
+            tournament.getTeams().add(team);
+
+            User player = new User();
+
+            player.setForename("User" + i);
+
+            player =  userRepository.save(player);
+
+            UserTournamentRole userTournamentRole = new UserTournamentRole();
+
+            userTournamentRole.setRole(UserRole.PLAYER);
+            userTournamentRole.setUser(player);
+            userTournamentRole.setTournament(tournament);
+            userTournamentRole.setTeam(team);
+
+            player.getUserTournamentRoles().add(userTournamentRole);
+            tournament.getUserTournamentRoles().add(userTournamentRole);
+            team.getUserTournamentRoles().add(userTournamentRole);
+
+            team = teamRepository.save(team);
+            userTournamentRoleRepository.save(userTournamentRole);
+            tournamentRepository.save(tournament);
+            userRepository.save(player);
+
+        }
+
+        HashMap<Round, List<Match>> results =  phaseService.generate(pool.getId().toString());
+        Assertions.assertEquals(1, tournament.getPhases().size(), "Tournament should have 1 phase");
+        Assertions.assertEquals(true, tournament.getPhases().get(0) instanceof Pool,
+                " It should be a instance of pool");
+        Assertions.assertEquals(8, tournament.getTeams().size(), " There should be 8 teams ");
+        Assertions.assertEquals(1, tournament.getTeams().get(0).getUserTournamentRoles().size(),
+                " There should be 1 player per team ");
+        Assertions.assertEquals(2, results.size(), " There should be 2 rounds of pool ");
+
+        for(Map.Entry<Round, List<Match>> entry : results.entrySet()){
+
+            Assertions.assertEquals(PhaseType.POOL, entry.getKey().getType(),
+                    " The round should be of type pool ");
+            Assertions.assertEquals(4, entry.getKey().getTeams().size(), " The  should be 4 teams");
+            Assertions.assertEquals(6, entry.getValue().size(), " The  should be 6 matches");
+
+        }
+
+    }
 }
