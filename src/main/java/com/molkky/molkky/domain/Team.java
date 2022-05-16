4721133561136f84360bde17e6ad7ca053c555ad
Aurélien MASSON <aurelien.masson@reseau.eseo.fr>
2022-05-16 21:06:38 +0200

    Fix UserTournamentRole in Team Aurelien

--- src/main/java/com/molkky/molkky/domain/Team.java
+++ src/main/java/com/molkky/molkky/domain/Team.java
@@ -35,7 +35,7 @@ public class Team implements Serializable {
     @ManyToMany(mappedBy = "teams")
     private List<Round> rounds;
 
-    @OneToMany(mappedBy="team")
+    @OneToMany(mappedBy="team",fetch = FetchType.EAGER)
     private List<UserTournamentRole> userTournamentRoles;
 
     @ManyToOne
