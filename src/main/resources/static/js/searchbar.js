0a1faafc8828bb4c1d553841db23322fbb12236e
lucien <lucien.voltz@reseau.eseo.fr>
2022-05-25 16:56:06 +0200

    First searchbar try

---
+++ src/main/resources/static/js/searchbar.js
@@ -0,0 +1,13 @@
+function getResults(){
+    var input = document.getElementById("searchbarInput").value;
+    var autoResultsContainer = document.getElementById("searchAutoresults");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            // Typical action to be performed when the document is ready:
+            autoResultsContainer.innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "/search/searchTournaments?term=" + input, true);
+    xhttp.send();
+}
