39f5973e3945f83587219889e7db9644a532d6a3
thuaulsa <sacha.thuault@reseau.eseo,fr>
2022-05-29 19:55:45 +0200

    CSS for team display

--- src/main/java/com/molkky/molkky/repository/TeamRepository.java
+++ src/main/java/com/molkky/molkky/repository/TeamRepository.java
@@ -1,9 +1,13 @@
 package com.molkky.molkky.repository;
 
 import com.molkky.molkky.domain.Team;
+import com.molkky.molkky.domain.Tournament;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, String>, JpaSpecificationExecutor<Team> {
