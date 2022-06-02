1325a9a449279cdefcff0cbcab88deed631e5fb0
thuaulsa <sacha.thuault@reseau.eseo,fr>
2022-06-02 16:16:51 +0200

    Coverage for InfosController

--- src/main/java/com/molkky/molkky/controllers/InfosController.java
+++ src/main/java/com/molkky/molkky/controllers/InfosController.java
@@ -17,6 +17,8 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class InfosController extends DefaultAttributes {
 
+    String redirect = "redirect:/infos";
+
     @Autowired
     UserRepository userRepository;
     @GetMapping("/infos")
@@ -26,47 +28,47 @@ public class InfosController extends DefaultAttributes {
 
     @PostMapping("/changePseudo")
     public String modifyPseudo(Model model,HttpSession session,@RequestParam(name="pseudo") String pseudo ){
-        UserLogged user = (UserLogged) session.getAttribute("user");
+        UserLogged user = getUser(session);
         User userDB = userRepository.findById(user.getId());
         userDB.setPseudo(pseudo);
         userRepository.save(userDB);
         user.setPseudo(pseudo);
         session.setAttribute("user",user);
-        return "redirect:/infos";
+        return redirect;
     }
     @PostMapping("/changeSurname")
     public String modifySurname(Model model,HttpSession session,@RequestParam(name="surname") String surname ){
-        UserLogged user = (UserLogged) session.getAttribute("user");
+        UserLogged user = getUser(session);
         User userDB = userRepository.findById(user.getId());
         userDB.setSurname(surname);
         userRepository.save(userDB);
         user.setSurname(surname);
         session.setAttribute("user",user);
-        return "redirect:/infos";
+        return redirect;
     }
 
     @PostMapping("/changeForename")
     public String modifyForename(Model model,HttpSession session,@RequestParam(name="forename") String forename ){
-        UserLogged user = (UserLogged) session.getAttribute("user");
+        UserLogged user = getUser(session);
         User userDB = userRepository.findById(user.getId());
         userDB.setForename(forename);
         userRepository.save(userDB);
         user.setForename(forename);
         session.setAttribute("user",user);
-        return "redirect:/infos";
+        return redirect;
     }
 
     @PostMapping("/changePassword")
     public String modifyPassword(Model model,HttpSession session,@RequestParam(name="pwd1") String pwd1, @RequestParam(name="pwd2") String pwd2 ){
         if(pwd1.equals(pwd2)) {
-            UserLogged user = (UserLogged) session.getAttribute("user");
+            UserLogged user = getUser(session);
             User userDB = userRepository.findById(user.getId());
             userDB.setPassword(pwd1);
             userRepository.save(userDB);
             user.setPassword(pwd2);
             session.setAttribute("user", user);
         }
-        return "redirect:/infos";
+        return redirect;
     }
 
 
--- src/test/java/com/molkky/molkky/controllers/InfosControllerTest.java
+++ src/test/java/com/molkky/molkky/controllers/InfosControllerTest.java
@@ -1,11 +1,16 @@
 package com.molkky.molkky.controllers;
 
 
+import com.molkky.molkky.domain.User;
+import com.molkky.molkky.domain.UserTournamentRole;
+import com.molkky.molkky.model.CreateTeamModel;
+import com.molkky.molkky.model.UserLogged;
 import com.molkky.molkky.repository.UserRepository;
 import com.molkky.molkky.repository.UserTournamentRoleRepository;
 import com.molkky.molkky.service.NotificationService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -13,7 +18,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockitoSession;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -38,6 +49,25 @@ class InfosControllerTest {
         mockMvc.perform(get("/infos/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("infos"));
+        UserLogged user = Mockito.mock(UserLogged.class);
+        when(user.getId()).thenReturn(1);
+        when(userRepository.findById(user.getId())).thenReturn(new User());
+        mockMvc.perform(post("/changePseudo").sessionAttr("user", user)
+                .param("pseudo", "tweew"))
+                .andExpect(view().name("redirect:/infos"));
+        mockMvc.perform(post("/changeSurname").sessionAttr("user", user)
+                        .param("surname", "test"))
+                .andExpect(view().name("redirect:/infos"));
+        mockMvc.perform(post("/changeForename").sessionAttr("user", user)
+                        .param("forename", "test"))
+                .andExpect(view().name("redirect:/infos"));
+        mockMvc.perform(post("/changePassword").sessionAttr("user", user)
+                        .param("pwd1", "test")
+                                .param("pwd2","test"))
+                .andExpect(view().name("redirect:/infos"));
+
     }
+
+
 
 }
