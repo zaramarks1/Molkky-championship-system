9eb2c55cc70f377273ae66e0442ce238ea2a9298
Louis RAMPAL <louis.rampal@gmail.com>
2022-06-03 13:48:40 +0200

    modif sidebar

---
+++ src/main/resources/static/js/sidebar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementsByClassName("sidebarContainerMobile")[0].style.left = "0px";
+}
+
+function closeNav() {
+    document.getElementsByClassName("sidebarContainerMobile")[0].style.left = "-250px";
+}
