df4a6feb6930f47535bb0ffac6bce1d7ae95e695
lucien <lucien.voltz@reseau.eseo.fr>
2022-06-07 11:26:25 +0200

    Responsive displayUsers

--- src/main/java/com/molkky/molkky/controllers/DisplayTeamController.java
+++ src/main/java/com/molkky/molkky/controllers/DisplayTeamController.java
@@ -19,8 +19,6 @@ public class DisplayTeamController extends DefaultAttributes {
 
     @GetMapping("/displayTeams")
     public String displayTeams(Model model, @RequestParam(value = "filter", required = false) String filter) {
-        TeamFilterModel teamModel = new TeamFilterModel();
-        model.addAttribute("teamModel", teamModel);
         if(filter != null && !"".equals(filter)){
             model.addAttribute("teams" , teamRepository.searchTeamsByName(filter, 10));
         } else {
@@ -28,16 +26,6 @@ public class DisplayTeamController extends DefaultAttributes {
         }
         return "team/displayTeams";
     }
-
-    @PostMapping("/filter")
-    public ModelAndView connexionUser(@ModelAttribute("teamModel")TeamFilterModel team2, Model model){
-        if(Boolean.TRUE.equals(teamRepository.existsTeamByName(team2.getName()))){
-            model.addAttribute("teams", teamRepository.findTeamByName(team2.getName()));
-            return new ModelAndView("/team/displayTeams");
-        }
-        return new ModelAndView("/team/displayTeams");
-    }
-
 }
 
 
