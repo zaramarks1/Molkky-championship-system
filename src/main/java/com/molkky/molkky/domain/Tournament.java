2fee7b8ef78654fb467fd124bc0a18fe8ce1df9a
Zara Marks <zara.marks@reseau.eseo.fr>
2022-06-17 11:40:16 +0200

    fix tests of phases

--- src/main/java/com/molkky/molkky/domain/Tournament.java
+++ src/main/java/com/molkky/molkky/domain/Tournament.java
@@ -9,7 +9,6 @@ import type.TournamentStatus;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
