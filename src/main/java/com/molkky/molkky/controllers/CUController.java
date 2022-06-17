8b196ac6a40d87ffaf9a37dbb6e7fda0e9188a76
Simon Duperray <64204457+SimonDuperray@users.noreply.github.com>
2022-06-17 11:37:08 +0200

    :sparkles: add fragments to CUPC views

---
+++ src/main/java/com/molkky/molkky/controllers/CUController.java
@@ -0,0 +1,14 @@
+package com.molkky.molkky.controllers;
+
+import com.molkky.molkky.controllers.superclass.DefaultAttributes;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class CUController extends DefaultAttributes {
+    @GetMapping("/conditionsUtilisation")
+    public String index(Model model, HttpSession session) { return "conditionsUtilisation"; }
+}
