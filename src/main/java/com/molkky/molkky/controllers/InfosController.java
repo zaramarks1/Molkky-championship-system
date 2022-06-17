d5e1b7d4ea9eee5d76fb247cd3ef919252ab229b
Simon Duperray <64204457+SimonDuperray@users.noreply.github.com>
2022-06-17 16:39:31 +0200

    provide matches from controller nok

--- src/main/java/com/molkky/molkky/controllers/InfosController.java
+++ src/main/java/com/molkky/molkky/controllers/InfosController.java
@@ -1,18 +1,26 @@
 package com.molkky.molkky.controllers;
 
 import com.molkky.molkky.controllers.superclass.DefaultAttributes;
+import com.molkky.molkky.domain.Match;
+import com.molkky.molkky.repository.MatchRepository;
+import com.molkky.molkky.repository.TournamentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class InfosController extends DefaultAttributes {
 
+    @Autowired
+    private MatchRepository matchRepository;
+
     @GetMapping("/infos")
     public String index(Model model, HttpSession session){
-
+        //List<Match> matches = matchRepository.findMatchAttributedToStaff();
         return "infos";
     }
 }
