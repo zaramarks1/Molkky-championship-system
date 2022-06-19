cf055c111c7572cd163c06c3f965eb37803130dd
Zara Marks <zara.marks@reseau.eseo.fr>
2022-06-19 18:18:08 +0200

    fix controller after merge

--- src/main/java/com/molkky/molkky/controllers/InfosController.java
+++ src/main/java/com/molkky/molkky/controllers/InfosController.java
@@ -1,14 +1,9 @@
 package com.molkky.molkky.controllers;
 
 import com.molkky.molkky.controllers.superclass.DefaultAttributes;
-
-import com.molkky.molkky.domain.Match;
-import com.molkky.molkky.domain.Tournament;
 import com.molkky.molkky.domain.User;
 import com.molkky.molkky.domain.UserTournamentRole;
 import com.molkky.molkky.model.UserLogged;
-import com.molkky.molkky.repository.MatchRepository;
-import com.molkky.molkky.repository.TournamentRepository;
 import com.molkky.molkky.repository.UserRepository;
 import com.molkky.molkky.repository.UserTournamentRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,43 +20,16 @@ import java.util.List;
 @Controller
 public class InfosController extends DefaultAttributes {
 
-
-    @Autowired
-    private MatchRepository matchRepository;
-
-    @Autowired
-    private UserTournamentRoleRepository userTournamentRoleRepository;
-
-    @Autowired
-    private TournamentRepository tournamentRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
     String redirect = "redirect:/infos";
 
 
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    UserTournamentRoleRepository userTournamentRoleRepository;
     @GetMapping("/infos")
     public String index(Model model, HttpSession session){
-        UserLogged userLogged = (UserLogged) session.getAttribute("user");
-        User loggedUser = userRepository.findById(userLogged.getId());
-
-        UserTournamentRole userTournamentRole = userTournamentRoleRepository.findUserTournamentRoleByUserId(loggedUser.getId());
-        Tournament tournament = tournamentRepository.findById(userTournamentRole.getTournament().getId());
-
-        String modifiedPseudo = loggedUser.getForename();
-        if(loggedUser.getForename() == null) {
-            modifiedPseudo = loggedUser.getForename()+"-X";
-        }
-
-        List<Match> matchList = matchRepository.findMatchAttributedToStaff(
-                tournament,
-                loggedUser
-        );
-
-        session.setAttribute("forename", modifiedPseudo);
-        model.addAttribute(matchList);
-
         return "infos";
     }
 
